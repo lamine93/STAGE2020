ffa7fdbd2928dbf3e912e25ea3152d6fc703c8fa
lamine93 <58446623+lamine93@users.noreply.github.com>
2020-11-28 03:39:13 +0100

    Update extract.sh

--- src/extract.sh
+++ src/extract.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-if [ $# -lt 3 ]; then
-    echo "./extract.sh  INFILE OUTFILE read "
+if [ $# -lt 1 ]; then
+    echo "./extract.sh <type>: read or write or rwrite ou rread"
     exit 1
 fi
 
 
-INFILE="$1"
-OUTFILE="$2"
-OUTDIR="$3"
-INDEX="$4"
+type="$1"
 
-
-if [ ! -d $OUTDIR ]; then
-    mkdir -p $OUTDIR
-fi
+BLOCK_SIZE=(4KB 16KB 64KB)
+#if [ ! -d $OUTDIR ]; then
+#    mkdir -p $OUTDIR
+#fi
 
 #'N;$!P;$!D;
-
-if [ $INDEX = "read" ]; then
-   cat  $INFILE | sed '1d'| sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > $OUTFILE
-   sed -i -e "s/d3/d/g" $OUTFILE
-   sed -i '1itemps type taille latence' $OUTFILE
-   mv $OUTFILE $OUTDIR
-fi
-
-if [ $INDEX = "write" ]; then
-   cat  $INFILE | sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > $OUTFILE
-   sed -i -e "s/e3/e/g" $OUTFILE
-   sed -i '1itemps type taille latence' $OUTFILE
-   mv $OUTFILE $OUTDIR
-fi
+for block_size in "${BLOCK_SIZE[@]}"; do
+    if [ $type = "read" ]; then
+       cat  "trace_$type$block_size.txt" | sed '1d'| sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > "trace_$type$block_size.csv"
+       sed -i -e "s/d3/d/g" "trace_$type$block_size.csv"
+       sed -i '1itemps type taille latence' "trace_$type$block_size.csv"
+    fi
+    if [ $type = "rread" ]; then
+       cat  "trace_$type$block_size.txt" | sed '1d'| sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > "trace_$type$block_size.csv"
+       sed -i -e "s/d3/d/g" "trace_$type$block_size.csv"
+       sed -i '1itemps type taille latence' "trace_$type$block_size.csv"
+    fi
+    if [ $type = "write" ]; then
+       cat  "trace_$type$block_size.txt" | sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > "trace_$type$block_size.csv"
+       sed -i -e "s/e3/e/g" "trace_$type$block_size.csv"
+       sed -i '1itemps type taille latence' "trace_$type$block_size.csv"
+    fi
+    if [ $type = "rwrite" ]; then
+       cat  "trace_$type$block_size.txt" | sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > "trace_$type$block_size.csv"
+       sed -i -e "s/e3/e/g" "trace_$type$block_size.csv"
+       sed -i '1itemps type taille latence' "trace_$type$block_size.csv"
+    fi
+    mv *.csv  "data_$type"/
+done
