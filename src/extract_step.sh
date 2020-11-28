ab8530ff791674a41364dc6e57935a401a19e5d4
lamine93 <58446623+lamine93@users.noreply.github.com>
2020-11-28 03:43:00 +0100

    Update extract_step.sh

--- src/extract_step.sh
+++ src/extract_step.sh
@@ -10,27 +10,30 @@ fi
 #OUTDIR="$3"
 type="$1"
 
-#TYPES=(write read)
-BLOCK_SIZE=(4B 16B 64B)
+TYPES=(write read)
+BLOCK_SIZE=(4KB 16KB 64KB)
 STEPS=(4 16 32 64)
+
+#for type in "${TYPES[@]}"; do
+#  for block_size in "${BLOCK_SIZE[@]}"; do
+#      mkdir -p  "data_$type$block_size.step"/
+#  done
+#done
 
 
 for block_size in "${BLOCK_SIZE[@]}"; do
   for step in "${STEPS[@]}"; do
     if [ $type = "read" ]; then
-       #INFILE = "trace_$type$block_size'step'$step.txt"
        cat  "trace_$type$block_size.step$step.txt" | sed '1d'| sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > "trace_$type$block_size.step$step.csv"
        sed -i -e "s/d3/d/g" "trace_$type$block_size.step$step.csv"
        sed -i '1itemps type taille latence' "trace_$type$block_size.step$step.csv"
     fi
     if [ $type = "write" ]; then
-       #INFILE = "trace_$type$block_size'step'$step.txt"
        cat  "trace_$type$block_size.step$step.txt" | sed '$d' | awk -v FS=" " -v OFS=" " '{print $1, $2, $6, $7}' | tr -d "'<';'>';'(';','" > "trace_$type$block_size.step$step.csv"
        sed -i -e "s/e3/e/g" "trace_$type$block_size.step$step.csv"
        sed -i '1itemps type taille latence' "trace_$type$block_size.step$step.csv"
     fi
   done
-
   mv *.csv "data_$type$block_size.step"/
 done
 
